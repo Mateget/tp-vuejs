2290badb6123e215704b4a72141b567aafa0c9f3
Gethen Matthieu <mgethen@iutbayonne.univ-pau.fr>
2021-03-22 16:35:45 +0100

    save

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,18 +5,20 @@
       <li v-for="doodle in doodles" :key="doodle.id">{{ doodle.name }}</li>
     </ul-->
 
-    <div>
-      <div class="card" v-for="doodle in doodles" :key="doodle.id">
-        {{ doodle.name }}
-      </div>
-      <div class="card" style="width: 18rem" v-for="doodle in doodles" :key="doodle.id">
-        <img class="card-img-top" src="..." alt="Card image cap" />
+    <div class="d-flex flex-row flex-wrap">
+      <div
+        class="card m-3"
+        style="width: 18rem"
+        v-for="doodle in doodles"
+        :key="doodle.id"
+      >
         <div class="card-body">
           <h5 class="card-title">{{ doodle.name }}</h5>
           <p class="card-text">
-            {{ doodle. }}
+            <!--{{ dateCreated_at(doodle) }}-->
+            Date : {{ doodle.createDate }}
           </p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <a href="#" class="btn btn-primary">Link</a>
         </div>
       </div>
     </div>
@@ -43,10 +45,20 @@ export default {
         .get("doodles")
         .then((response) => {
           this.doodles = response.data;
+          var options = {weekday: "long", year: "numeric", month: "long", day: "numeric"};
+          this.doodles.forEach( (doodle) => {
+              doodle.createDate = new Date(doodle.created_at*1000).toLocaleDateString('fr-FR',options);
+          })
         })
         .finally(() => {
           this.loading = false;
         });
+    },
+    dateCreated_at(doodle) {
+      console.log(doodle.created_at);
+      var date = new Date(doodle.created_at);
+      console.log(date);
+      return date.toDateString;
     },
   },
 };
