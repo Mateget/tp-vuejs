60b8216b606fd134fa8130fe5f14454c9f229522
Gethen Matthieu <mgethen@iutbayonne.univ-pau.fr>
2021-03-22 16:06:28 +0100

    save

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-main">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-between" id="navbar-main">
+          <div class="navbar-nav">
+            <router-link to="/" class="navbar-brand">Home</router-link>
+          </div>
+          <ul class="navbar-nav mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link to="/page1" class="nav-link">Page 1</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/page2" class="nav-link">Page 2</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container mt-4">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+
+<script>
+const pages = {
+  accueil: {
+    title: 'Page accueil'
+  },
+  page1: {
+    title: 'Page 1'
+  },
+  page2: {
+    title: 'Page 2'
+  }
+};
+
+export default {
+  data() {
+    return {
+      currentPage: 'accueil'
+    }
+  },
+  computed: {
+    title() {
+      return pages[this.currentPage].title;
+    },
+  },
+  methods: {
+    page(id) {
+      this.currentPage = id;
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <h1>Dessins</h1>
+    <!--ul>
+      <li v-for="doodle in doodles" :key="doodle.id">{{ doodle.name }}</li>
+    </ul-->
+
+    <div>
+      <div class="card" v-for="doodle in doodles" :key="doodle.id">
+        {{ doodle.name }}
+      </div>
+      <div class="card" style="width: 18rem" v-for="doodle in doodles" :key="doodle.id">
+        <img class="card-img-top" src="..." alt="Card image cap" />
+        <div class="card-body">
+          <h5 class="card-title">{{ doodle.name }}</h5>
+          <p class="card-text">
+            {{ doodle. }}
+          </p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import api from "../api.js";
+
+export default {
+  data() {
+    return {
+      loading: false,
+      doodles: [],
+    };
+  },
+  mounted() {
+    this.refreshDoodles();
+  },
+  methods: {
+    refreshDoodles() {
+      this.loading = true;
+      api
+        .get("doodles")
+        .then((response) => {
+          this.doodles = response.data;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+  },
+};
+</script>
